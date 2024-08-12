bbf99035d3a54c522a15feaf6ac6251f489539fc
Aditya Amit Trivedi <128981641+adityaamit18@users.noreply.github.com>
2024-08-12 21:21:34 +0530

    Add files via upload

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+/*output
+true
+true
+false
+false
+true */
+
+
+
+// console.log("2">1); //true
+// console.log("02">1); //true
+
+
+
+console.log(null > 0); //f
+console.log(null == 0); //f
+console.log(null >= 0); //t
+
+
+console.log(undefined == 0); //undefined always gives false
+
+// === Triple Equal checks Datatype too
+console.log("2"=== 2); //false
+console.log("2"== 2); //true
